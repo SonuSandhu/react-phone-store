df2ec423ce22326d81465c1d0d549d22c3801c2d
Baljinder Singh <sonu.best@hotmail.com>
2019-08-27 15:02:46 +0530

    first commit

---
+++ src/Data.js
@@ -0,0 +1,107 @@
+
+export const storeProduct = [
+    {
+        id: 1,
+        title: "Samsung",
+        img: "asserts/images.jpg",
+        price: 13,
+        company: "Samsung",
+        info:"eiusmod tempor incididunt ut labore et dolore magna aliqua. Est ullamcorper eget nulla facilisi etiam dignissim. Aliquet nec ullamcorper sit amet risus nullam eget felis eget. Rhoncus dolor purus non enim praesent. Non odio euismod lacinia at quis. Neque gravida in fermentum et sollicitudin ac orci phasellus egestas. Id eu nisl nunc mi ipsum faucibus vitae aliquet nec. Vel pretium lectus quam id. Euismod lacinia at quis risus sed. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Commodo nulla facilisi nullam vehicula. Scelerisque viverra mauris in aliquam sem fringilla ut morbi. Massa tempor nec feugiat nisl pretium fusce id velit ut. Erat pellentesque adipiscing commodo elit. Non consectetur a erat nam at lectus urna duis convallis. Euismod in pellentesque massa placerat duis ultricies lacus. Nunc scelerisque viverra mauris in aliquam sem fringilla.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 2,
+        title: "Vivo",
+        img: "asserts/Black - Vivo.jpg",
+        price: 16,
+        company: "VIVO",
+        info:"eiusmod tempor incididunt ut labore et dolore magna aliqua. Est ullamcorper eget nulla facilisi etiam dignissim. Aliquet nec ullamcorper sit amet risus nullam eget felis eget. Rhoncus dolor purus non enim praesent. Non odio euismod lacinia at quis. Neque gravida in fermentum et sollicitudin ac orci phasellus egestas. Id eu nisl nunc mi ipsum faucibus vitae aliquet nec. Vel pretium lectus quam id. Euismod lacinia at quis risus sed. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Commodo nulla facilisi nullam vehicula. Scelerisque viverra mauris in aliquam sem fringilla ut morbi. Massa tempor nec feugiat nisl pretium fusce id velit ut. Erat pellentesque adipiscing commodo elit. Non consectetur a erat nam at lectus urna duis convallis. Euismod in pellentesque massa placerat duis ultricies lacus. Nunc scelerisque viverra mauris in aliquam sem fringilla.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 3,
+        title: "I Phone",
+        img: "asserts/iphn.jpg",
+        price: 18,
+        company: "APPLE",
+        info:"eiusmod tempor incididunt ut labore et dolore magna aliqua. Est ullamcorper eget nulla facilisi etiam dignissim. Aliquet nec ullamcorper sit amet risus nullam eget felis eget. Rhoncus dolor purus non enim praesent. Non odio euismod lacinia at quis. Neque gravida in fermentum et sollicitudin ac orci phasellus egestas. Id eu nisl nunc mi ipsum faucibus vitae aliquet nec. Vel pretium lectus quam id. Euismod lacinia at quis risus sed. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Commodo nulla facilisi nullam vehicula. Scelerisque viverra mauris in aliquam sem fringilla ut morbi. Massa tempor nec feugiat nisl pretium fusce id velit ut. Erat pellentesque adipiscing commodo elit. Non consectetur a erat nam at lectus urna duis convallis. Euismod in pellentesque massa placerat duis ultricies lacus. Nunc scelerisque viverra mauris in aliquam sem fringilla.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 4,
+        title: "Mcr-mx",
+        img: "asserts/micromax.jpg",
+        price: 12,
+        company: "MICROMEX",
+        info:"eiusmod tempor incididunt ut labore et dolore magna aliqua. Est ullamcorper eget nulla facilisi etiam dignissim. Aliquet nec ullamcorper sit amet risus nullam eget felis eget. Rhoncus dolor purus non enim praesent. Non odio euismod lacinia at quis. Neque gravida in fermentum et sollicitudin ac orci phasellus egestas. Id eu nisl nunc mi ipsum faucibus vitae aliquet nec. Vel pretium lectus quam id. Euismod lacinia at quis risus sed. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Commodo nulla facilisi nullam vehicula. Scelerisque viverra mauris in aliquam sem fringilla ut morbi. Massa tempor nec feugiat nisl pretium fusce id velit ut. Erat pellentesque adipiscing commodo elit. Non consectetur a erat nam at lectus urna duis convallis. Euismod in pellentesque massa placerat duis ultricies lacus. Nunc scelerisque viverra mauris in aliquam sem fringilla.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },{
+        id: 5,
+        title: "nokia-340",
+        img: "asserts/nokia.jpg",
+        price: 14,
+        company: "MICROSOFT",
+        info:"eiusmod tempor incididunt ut labore et dolore magna aliqua. Est ullamcorper eget nulla facilisi etiam dignissim. Aliquet nec ullamcorper sit amet risus nullam eget felis eget. Rhoncus dolor purus non enim praesent. Non odio euismod lacinia at quis. Neque gravida in fermentum et sollicitudin ac orci phasellus egestas. Id eu nisl nunc mi ipsum faucibus vitae aliquet nec. Vel pretium lectus quam id. Euismod lacinia at quis risus sed. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Commodo nulla facilisi nullam vehicula. Scelerisque viverra mauris in aliquam sem fringilla ut morbi. Massa tempor nec feugiat nisl pretium fusce id velit ut. Erat pellentesque adipiscing commodo elit. Non consectetur a erat nam at lectus urna duis convallis. Euismod in pellentesque massa placerat duis ultricies lacus. Nunc scelerisque viverra mauris in aliquam sem fringilla.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 6,
+        title: "redme-xomi",
+        img: "asserts/redme.jpg",
+        price: 10,
+        company: "Samsung",
+        info:"eiusmod tempor incididunt ut labore et dolore magna aliqua. Est ullamcorper eget nulla facilisi etiam dignissim. Aliquet nec ullamcorper sit amet risus nullam eget felis eget. Rhoncus dolor purus non enim praesent. Non odio euismod lacinia at quis. Neque gravida in fermentum et sollicitudin ac orci phasellus egestas. Id eu nisl nunc mi ipsum faucibus vitae aliquet nec. Vel pretium lectus quam id. Euismod lacinia at quis risus sed. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Commodo nulla facilisi nullam vehicula. Scelerisque viverra mauris in aliquam sem fringilla ut morbi. Massa tempor nec feugiat nisl pretium fusce id velit ut. Erat pellentesque adipiscing commodo elit. Non consectetur a erat nam at lectus urna duis convallis. Euismod in pellentesque massa placerat duis ultricies lacus. Nunc scelerisque viverra mauris in aliquam sem fringilla.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 7,
+        title: "Xiomi",
+        img: "asserts/xiaomi-redmi.jpg",
+        price: 23,
+        company: "Samsung",
+        info:"eiusmod tempor incididunt ut labore et dolore magna aliqua. Est ullamcorper eget nulla facilisi etiam dignissim. Aliquet nec ullamcorper sit amet risus nullam eget felis eget. Rhoncus dolor purus non enim praesent. Non odio euismod lacinia at quis. Neque gravida in fermentum et sollicitudin ac orci phasellus egestas. Id eu nisl nunc mi ipsum faucibus vitae aliquet nec. Vel pretium lectus quam id. Euismod lacinia at quis risus sed. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Commodo nulla facilisi nullam vehicula. Scelerisque viverra mauris in aliquam sem fringilla ut morbi. Massa tempor nec feugiat nisl pretium fusce id velit ut. Erat pellentesque adipiscing commodo elit. Non consectetur a erat nam at lectus urna duis convallis. Euismod in pellentesque massa placerat duis ultricies lacus. Nunc scelerisque viverra mauris in aliquam sem fringilla.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 8,
+        title: "Samsung",
+        img: "asserts/images.jpg",
+        price: 20,
+        company: "Samsung",
+        info:"eiusmod tempor incididunt ut labore et dolore magna aliqua. Est ullamcorper eget nulla facilisi etiam dignissim. Aliquet nec ullamcorper sit amet risus nullam eget felis eget. Rhoncus dolor purus non enim praesent. Non odio euismod lacinia at quis. Neque gravida in fermentum et sollicitudin ac orci phasellus egestas. Id eu nisl nunc mi ipsum faucibus vitae aliquet nec. Vel pretium lectus quam id. Euismod lacinia at quis risus sed. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Commodo nulla facilisi nullam vehicula. Scelerisque viverra mauris in aliquam sem fringilla ut morbi. Massa tempor nec feugiat nisl pretium fusce id velit ut. Erat pellentesque adipiscing commodo elit. Non consectetur a erat nam at lectus urna duis convallis. Euismod in pellentesque massa placerat duis ultricies lacus. Nunc scelerisque viverra mauris in aliquam sem fringilla.",
+        inCart: false,
+        count: 0,
+        total: 0
+    }
+   
+]
+
+
+export const detailProduct = {
+  
+       id: 1,
+        title: "Xiomi",
+        img: "asserts/xiaomi-redmi.jpg",
+        price: 23,
+        company: "Samsung",
+        info: 
+            "eiusmod tempor incididunt ut labore et dolore magna aliqua. Est ullamcorper eget nulla facilisi etiam dignissim. Aliquet nec ullamcorper sit amet risus nullam eget felis eget. Rhoncus dolor purus non enim praesent. Non odio euismod lacinia at quis. Neque gravida in fermentum et sollicitudin ac orci phasellus egestas. Id eu nisl nunc mi ipsum faucibus vitae aliquet nec. Vel pretium lectus quam id. Euismod lacinia at quis risus sed. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Commodo nulla facilisi nullam vehicula. Scelerisque viverra mauris in aliquam sem fringilla ut morbi.",
+        inCart: false,
+        count: 0,
+        total: 0
+     
+};
